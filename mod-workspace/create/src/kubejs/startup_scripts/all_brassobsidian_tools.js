f9199a919e373d1a608f48b961adf08cd8fed82c
Shadrin Igor <igor@shadrins.ru>
2024-02-17 13:00:32 +0300

    Kubejs fix

--- mod-workspace/create/src/kubejs/startup_scripts/all_brassobsidian_tools.js
+++ mod-workspace/create/src/kubejs/startup_scripts/all_brassobsidian_tools.js
@@ -1,7 +1,47 @@
 StartupEvents.registry('item', e => {
-  e.create('brassobsidian_pickaxe').displayName('Латунно-обсидиановая кирка')
-  e.create('brassobsidian_shovel').displayName('Латунно-обсидиановая лопата')
-  e.create('brassobsidian_sword').displayName('Латунно-обсидиановый меч')
-  e.create('brassobsidian_hoe').displayName('Латунно-обсидиановая мотыга')  
-  e.create('brassobsidian_axe').displayName('Латунно-обсидиановый топор')
+  e.create('brassobsidian_pickaxe', 'pickaxe').displayName('Латунно-обсидиановая кирка').unstackable().modifyTier(tier => 
+  {
+    tier.uses = 1561
+    tier.speed = 12.0
+    tier.attackDamageBonus = 2.0
+    tier.level = 2
+    tier.enchantmentValue = 14
+  }
+  )
+  e.create('brassobsidian_shovel', 'shovel').displayName('Латунно-обсидиановая лопата').unstackable().modifyTier(tier => 
+  {
+    tier.uses = 1561
+    tier.speed = 12.0
+    tier.attackDamageBonus = 2.0
+    tier.level = 2
+    tier.enchantmentValue = 14
+  }
+  )
+  e.create('brassobsidian_sword', 'sword').displayName('Латунно-обсидиановый меч').unstackable().modifyTier(tier => 
+  {
+    tier.uses = 1561
+    tier.speed = 12.0
+    tier.attackDamageBonus = 9.0
+    tier.level = 2
+    tier.enchantmentValue = 14
+  }
+  )
+  e.create('brassobsidian_hoe', 'hoe').displayName('Латунно-обсидиановая мотыга').unstackable().modifyTier(tier => 
+  {
+    tier.uses = 1561
+    tier.speed = 12.0
+    tier.attackDamageBonus = 2.0
+    tier.level = 2
+    tier.enchantmentValue = 14
+  }
+  )
+  e.create('brassobsidian_axe', 'axe').displayName('Латунно-обсидиановый топор').unstackable().modifyTier(tier => 
+  {
+    tier.uses = 1561
+    tier.speed = 12.0
+    tier.attackDamageBonus = 10.0
+    tier.level = 2
+    tier.enchantmentValue = 14
+  }
+  )
 })
